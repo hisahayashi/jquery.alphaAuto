69b439d0e534cb182d55180f5bc3cfef74880e0d
hisahayashi <hisa.hayashi@gmail.com>
2015-03-20 00:32:42 +0900

    init

---
+++ jquery.alphaAuto.js
@@ -0,0 +1,43 @@
+;(function($) {
+  $.fn.alphaAuto = function(opacity, duration, callback) {
+    var options = {};
+    var d = {
+      opacity: 0,
+      duration: 0,
+      callback: null
+    };
+    var o = $.extend(d, options);
+    o.opacity = (opacity != undefined) ? opacity : o.opacity;
+    o.duration = (duration != undefined) ? duration : o.duration;
+    o.callback = ($.isFunction(callback)) ? callback : o.callback;
+
+    var $that = $(this);
+
+    $that.each(function(i) {
+      var $t = $(this);
+      if (o.opacity <= 0) {
+        $t.animate({
+          opacity: 0
+          , filter: 'alpha( opacity=0 );'
+        }, o.duration, 'linear', function() {
+          $t.css({
+            display: 'none'
+          });
+          if ($.isFunction(o.callback)) o.callback();
+        });
+      } else {
+        $t.css({
+          display: 'block'
+        });
+        $t.animate({
+          opacity: o.opacity
+          , filter: 'alpha( opacity=' + o.opacity + ' );'
+        }, o.duration, 'linear', function() {
+          if ($.isFunction(o.callback)) o.callback();
+        });
+      }
+    });
+
+    return $that;
+  };
+})(jQuery);
